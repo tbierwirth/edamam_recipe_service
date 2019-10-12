066d47df7d0b4e589e17037c0d173e053a73fe7b
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-12 14:11:58 -0600

    Create test for edamam service
    Co-Authored-By: Glynnis <oconnellg1717@gmail.com>

---
+++ tests/services/edamam_service.spec.js
@@ -0,0 +1,17 @@
+var app = require('../../app');
+var EdamamService = require('../../services/edamam_service').EdamamService
+
+describe('Edamam Service', () => {
+  test('should return response from Edamam Recipe Search API', () => {
+    let food = "chicken"
+    let edamam = new EdamamService(food)
+
+    return edamam.getRecipes()
+      .then(response => {
+        let results = response['hits'][0]
+        expect(results.recipe.label).toBe('Chicken Vesuvio')
+        expect(results.recipe.source).toBe('Serious Eats')
+        expect(results.recipe.yield).toBe(4.0)
+      })
+  });
+});
