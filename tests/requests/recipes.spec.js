00a143e4fce48df99cad13b95984ac6c76e9afcc
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-14 13:31:06 -0600

    Add before/after actions and adjust expectations for test
    Co-Authored-By: Glynnis <oconnellg1717@gmail.com>

--- tests/requests/recipes.spec.js
+++ tests/requests/recipes.spec.js
@@ -4,12 +4,26 @@ var request = require('supertest');
 var recipe = require('../../models').Recipe;
 
 describe('api', () => {
-  test('Test POST api/v1/recipes', () => {
-    return request(app).post('/api/v1/recipes').query({
-      food_type: 'chicken'
-    })
-    .then(response => {
-      expect(response.status).toBe(201)
+  beforeAll(() => {
+  shell.exec('npx sequelize db:create --env test')
+  shell.exec('npx sequelize db:migrate --env test')
+  });
+
+  afterAll(() => {
+    shell.exec('npx sequelize db:migrate:undo:all --env test')
+    });
+
+  describe('Test POST api/v1/recipes', () => {
+    test('should create recipes from Edamam', () => {
+      return request(app).post('/api/v1/recipes').query({
+        food_type: 'chicken'
+      })
+      .then(response => {
+        expect(response.status).toBe(201)
+        recipe.findAll().then(recipes => {
+          expect(recipes.length).toBe(10)
+        })
+      })
     })
   })
 })
