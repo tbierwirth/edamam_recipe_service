591898b5833e728786f901969f197a68396f35ae
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-14 13:31:49 -0600

    Fix how recipe info is nested
    Co-Authored-By: Glynnis <oconnellg1717@gmail.com>

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -6,31 +6,39 @@ var EdamamService = require('../../../services/edamam_service').EdamamService
 router.post('/', function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   var edamam = new EdamamService(req.query.food_type)
+  const recipes_created = []
   return edamam.getRecipes()
-  .then(response => {
-    if (response.hits === []) {
+  .then(async response => {
+    if (response.hits.length == 0) {
       res.status(404).send({message: "No recipes found"})
     } else {
       let results = response.hits
       for (var i = 0; i < results.length; i++) {
-        recipes.findOrCreate({where: { name: results[i].label},
-          recipeUrl: results[i].url,
-          calories: results[i].calories,
-          servings: results[i].yield,
-          carbohydrates: results[i].digest[1].total,
-          protein: results[i].digest[2].total,
-          fat: results[i].digest[0].total,
-          ingredientCount: results[i].ingredients.length,
-          prepTime: results[i].totalTime
+        await recipes.findOrCreate({
+          where: {
+            name: results[i].recipe.label
+          },
+          defaults: {
+            name: results[i].recipe.label,
+            recipeUrl: results[i].recipe.url,
+            calories: results[i].recipe.calories,
+            servings: results[i].recipe.yield,
+            carbohydrates: results[i].recipe.digest[1].total,
+            protein: results[i].recipe.digest[2].total,
+            fat: results[i].recipe.digest[0].total,
+            ingredientCount: results[i].recipe.ingredients.length,
+            prepTime: results[i].recipe.totalTime
+          }
         })
         .then(([recipe, created]) => {
-          console.log(recipe.get({plain: true}))
-          console.log(created)
-      })
+          recipes_created.push(recipe.get({ plain: true }))
+        })
+      }
     }
   })
+  .then(response => {
+    res.status(201).send(recipes_created)
+  })
 })
-
-
 
 module.exports = router;
